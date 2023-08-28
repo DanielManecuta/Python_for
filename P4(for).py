e1f6f2c4d585f4a5065615ef25f7cfcd8c53197e
DanielManecuta <143198345+DanielManecuta@users.noreply.github.com>
2023-08-28 22:14:59 +0300

    Add files via upload

---
+++ P4(for).py
@@ -0,0 +1,34 @@
+# list = ['Mosh','Josh','Sarah'];
+# print (list)
+
+# for item in range(10):
+#     print(item)
+# for item in range(5,10):
+#     print(item)
+# for item in range(5,10,2):
+#     print(item)
+
+# prices = [10, 20, 30]
+# sum = 0
+
+# for item in prices:
+#     sum += item
+# print(f"Totalul tuturor lucrurilor cumparate este {sum}.")
+
+#Coordonate
+
+# for x in range(4):
+#     for y in range(3):
+#         print(f"({x},{y})")
+
+numbers = [2, 2, 2, 2, 5]
+
+# for item in numbers:
+#     print("X"*item)
+
+# for item in numbers:
+#     output = 'x'
+#     for count in range(item):
+#         print(output,end = "")
+#     print()
+
